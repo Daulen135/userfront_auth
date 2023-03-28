a2722befd888b7337b5a98e6446a80561635333d
daulen <daulenz@gmail.com>
2023-03-27 21:24:23 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import Userfront from "@userfront/react";
 
-function App() {
+Userfront.init("demo1234");
+
+const SignupForm = Userfront.build({
+  toolId: "nkmbbm",
+});
+const LoginForm = Userfront.build({
+  toolId: "alnkkd",
+});
+const PasswordResetForm = Userfront.build({
+  toolId: "dkbmmo",
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/reset">Reset</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<PasswordReset />} />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <SignupForm />
+     
     </div>
   );
 }
 
-export default App;
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+function PasswordReset() {
+  return (
+    <div>
+      <h2>Password Reset</h2>
+      <PasswordResetForm />
+    </div>
+  );
+}
+
+function Dashboard() {
+  const [privateData, setPrivateData] = useState({ data: '' });
+
+  // Get private data
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await fetch('http://localhost:3010/data', {
+          headers: {
+            Authorization: `Bearer ${Userfront.accessToken()}`,
+          },
+        }).then((response) => response.json());
+        setPrivateData(result);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  const userData = JSON.stringify(Userfront.user, null, 2);
+  console.log(Userfront)
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <pre>{userData}</pre>
+      <button onClick={Userfront.logout}>Logout</button>
+      <h3>Private data</h3>
+      <pre>{JSON.stringify(privateData, null, 2)}</pre>
+    </div>
+  );
+}
+
+function RequireAuth({ children }) {
+  let location = useLocation();
+  if (!Userfront.tokens.accessToken) {
+    // Redirect to the /login page
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
